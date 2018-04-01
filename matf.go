3ba4b0879c5209cf3db4a74bbf49e4550e6bf53d
Lehner Florian <dev@der-flo.net>
2018-04-01 21:35:04 +0200

    Start implementing MiMatrix
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- matf.go
+++ matf.go
@@ -33,6 +33,14 @@ type Matf struct {
 	byteSwapping bool
 }
 
+// MatMatrix represents a matrix
+type MatMatrix struct {
+	Name          string
+	Flags         uint32
+	RealPart      interface{}
+	ImaginaryPart interface{}
+}
+
 // Header contains informations about the MAT-file
 type Header struct {
 	Text                string // 0 - 116
@@ -65,36 +73,173 @@ func readHeader(mat *Matf, file *os.File) error {
 	return nil
 }
 
-func smallDataElementField(data []byte, order binary.ByteOrder) (int, interface{}, error) {
-	//numberOfBytes := order.Uint16(data[:2])
-	dataType := order.Uint16(data[2:4])
+func isSmallDataElementFormat(data []byte, order binary.ByteOrder) (bool, error) {
+	var offset int
 
-	return int(dataType), nil, nil
+	if order == binary.LittleEndian {
+		offset = 2
+	}
+
+	small := make([]byte, 2)
+	buf := bytes.NewReader(data[offset:])
+	if err := binary.Read(buf, order, &small); err != nil {
+		return false, fmt.Errorf("Could not read bytes:", err)
+	}
+	if small[0] != small[1] {
+		// Small Data Element Format
+		return true, nil
+	}
+	return false, nil
 }
 
-func readDataElement(m *Matf, order binary.ByteOrder) (int, interface{}, error) {
-	data := make([]byte, 8)
+func checkIndex(index int) int {
+	for {
+		if index%8 == 0 {
+			return index
+		}
+		index++
+	}
+}
+
+func extractMatrix(data []byte, order binary.ByteOrder) (MatMatrix, error) {
+	var matrix MatMatrix
+	fmt.Println("extractMatrix()")
+	var index int
+	var offset int
+	var dataType uint32
+	var numberOfBytes uint32
+	var small bool
+	var err error
+	var buf *bytes.Reader
+
+	// Array Flags
+	small, err = isSmallDataElementFormat(data[index:], order)
+	if err != nil {
+		return MatMatrix{}, err
+	}
+	if small {
+		//dataType = uint32(order.Uint16(data[index+0 : index+2]))
+		numberOfBytes = uint32(order.Uint16(data[index+2 : index+4]))
+		offset = 4
+	} else {
+		//dataType = order.Uint32(data[index+0 : index+4])
+		numberOfBytes = order.Uint32(data[index+4 : index+8])
+		offset = 8
+	}
+	arrayFlags := make([]byte, int(numberOfBytes))
+	buf = bytes.NewReader(data[index+offset:])
+	if err := binary.Read(buf, order, &arrayFlags); err != nil {
+		return MatMatrix{}, err
+	}
+	matrix.Flags = binary.LittleEndian.Uint32(arrayFlags)
+	fmt.Printf("Array Flags:\t%v\t%v\n", arrayFlags, matrix.Flags)
+	index += (offset + int(numberOfBytes))
+	index = checkIndex(index)
+
+	// Dimensions Array
+	small, err = isSmallDataElementFormat(data[index:], order)
+	if err != nil {
+		return MatMatrix{}, err
+	}
+	if small {
+		//dataType = uint32(order.Uint16(data[index+0 : index+2]))
+		numberOfBytes = uint32(order.Uint16(data[index+2 : index+4]))
+		offset = 4
+	} else {
+		//dataType = order.Uint32(data[index+0 : index+4])
+		numberOfBytes = order.Uint32(data[index+4 : index+8])
+		offset = 8
+	}
+	dimArray := make([]byte, int(numberOfBytes))
+	buf = bytes.NewReader(data[index+offset:])
+	if err := binary.Read(buf, order, &dimArray); err != nil {
+		return MatMatrix{}, err
+	}
+	fmt.Printf("Dimensions Array:\t%v\n", dimArray)
+	index += (offset + int(numberOfBytes))
+	index = checkIndex(index)
+
+	// Array Name
+	small, err = isSmallDataElementFormat(data[index:], order)
+	if err != nil {
+		return MatMatrix{}, err
+	}
+	if small {
+		//dataType = uint32(order.Uint16(data[index+0 : index+2]))
+		numberOfBytes = uint32(order.Uint16(data[index+2 : index+4]))
+		offset = 4
+	} else {
+		//dataType = order.Uint32(data[index+0 : index+4])
+		numberOfBytes = order.Uint32(data[index+4 : index+8])
+		offset = 8
+	}
+	arrayName := make([]byte, int(numberOfBytes))
+	buf = bytes.NewReader(data[index+offset:])
+	if err := binary.Read(buf, order, &arrayName); err != nil {
+		return MatMatrix{}, err
+	}
+	matrix.Name = string(arrayName)
+	fmt.Printf("Array Name:\t%v\t%v\n", arrayName, matrix.Name)
+	index += (offset + int(numberOfBytes))
+	index = checkIndex(index)
+
+	// Real part
+	small, err = isSmallDataElementFormat(data[index:], order)
+	if err != nil {
+		return MatMatrix{}, err
+	}
+	if small {
+		dataType = uint32(order.Uint16(data[index+0 : index+2]))
+		numberOfBytes = uint32(order.Uint16(data[index+2 : index+4]))
+		offset = 4
+	} else {
+		dataType = order.Uint32(data[index+0 : index+4])
+		numberOfBytes = order.Uint32(data[index+4 : index+8])
+		offset = 8
+	}
+	fmt.Println("Realpart:\t", dataType, numberOfBytes)
+
+	/*
+		// Imaginary part (optional)
+	*/
+	return matrix, nil
+}
+
+func readBytes(m *Matf, numberOfBytes int) ([]byte, error) {
+	data := make([]byte, numberOfBytes)
 	count, err := m.file.Read(data)
+	if err != nil {
+		return nil, err
+	}
+
+	if count != numberOfBytes {
+		return nil, fmt.Errorf("Could not read enough bytes")
+	}
+	return data, nil
+}
+
+func readDataElementField(m *Matf, order binary.ByteOrder) (int, interface{}, error) {
+	tag, err := readBytes(m, 8)
 	if err != nil {
 		return 0, nil, err
 	}
 
-	if count != 8 {
-		return 0, nil, fmt.Errorf("Could not read enough bytes")
+	dataType := order.Uint32(tag[:4])
+	numberOfBytes := order.Uint32(tag[4:8])
+
+	fmt.Println("DataType: ", dataType, "\tNumberOfBytes: ", numberOfBytes)
+
+	data, err := readBytes(m, int(numberOfBytes))
+	if err != nil {
+		return 0, nil, err
 	}
 
-	small := make([]byte, 2)
-	buf := bytes.NewReader(data)
-	if err := binary.Read(buf, order, &small); err != nil {
-		return 0, nil, fmt.Errorf("Could not read bytes:", err)
+	switch int(dataType) {
+	case MiCompressed:
+		return 0, nil, fmt.Errorf("MiCompressed is not yet implemented")
+	case MiMatrix:
+		extractMatrix(data, order)
 	}
-	if small[0] == 0 && small[1] == 0 {
-		// Small Data Element Format
-		return smallDataElementField(data, order)
-	}
-
-	dataType := order.Uint32(data[:4])
-	//numberOfBytes := order.Uint32(data[4:8])
 
 	return int(dataType), nil, nil
 }
@@ -117,9 +262,9 @@ func Open(file string) (*Matf, error) {
 // ReadDataElement returns the next data element
 func ReadDataElement(file *Matf) (int, interface{}, error) {
 	if file.byteSwapping == true {
-		return readDataElement(file, binary.LittleEndian)
+		return readDataElementField(file, binary.LittleEndian)
 	}
-	return readDataElement(file, binary.BigEndian)
+	return readDataElementField(file, binary.BigEndian)
 
 }
 
