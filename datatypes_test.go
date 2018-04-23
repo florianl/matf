70f5fca70dc85691881b44cecda85efe3af717b9
Lehner Florian <dev@der-flo.net>
2018-04-23 20:29:02 +0200

    Add test for extractNumeric()
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- datatypes_test.go
+++ datatypes_test.go
@@ -11,13 +11,13 @@ func TestExtractDataElement(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		name string
-		data *[]byte
-		order binary.ByteOrder
-		dataType int
+		name          string
+		data          *[]byte
+		order         binary.ByteOrder
+		dataType      int
 		numberOfBytes int
-		ele interface{}
-		err  string
+		ele           interface{}
+		err           string
 	}{
 		{name: "Unknown", data: &([]byte{0x11, 0x22, 0x33, 0x44}), order: binary.LittleEndian, dataType: 42, numberOfBytes: 42, err: "is not supported"},
 		{name: "MiInt8", data: &([]byte{0x11, 0x22, 0x33, 0x44}), order: binary.LittleEndian, dataType: MiInt8, numberOfBytes: 1, ele: 17},
@@ -44,6 +44,36 @@ func TestExtractDataElement(t *testing.T) {
 				t.Fatalf("Expected error, got none")
 			}
 			fmt.Println(ele, tc.ele)
+		})
+	}
+}
+
+func TestExtractNumeric(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		data  *[]byte
+		order binary.ByteOrder
+		step  int
+		ele   interface{}
+		err   string
+	}{
+		{name: "[1,2]", data: &([]byte{0x09, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40}), order: binary.LittleEndian, step: 24, ele: []int{1, 2}},
+		{name: "SmallData", data: &([]byte{0x06, 0x00, 0x04, 0x00, 0x01, 0x03, 0x03, 0x07}), order: binary.LittleEndian, step: 8, ele: []int{117637889}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ele, step, err := extractNumeric(tc.data, tc.order)
+			if err != nil {
+				if matched, _ := regexp.MatchString(tc.err, err.Error()); matched == false {
+					t.Fatalf("Error matching regex: %v \t Got: %v", tc.err, err)
+				}
+			} else if len(tc.err) != 0 {
+				t.Fatalf("Expected error, got none")
+			}
+			fmt.Println(ele, tc.ele, "\t", step, tc.step)
 		})
 	}
 }
